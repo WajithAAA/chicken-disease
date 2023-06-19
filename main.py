c644a2b6b940d7b0e308cd0d94fb03976141de3f
WajithAAA <azeeswajid@gmail.com>
2023-06-19 23:13:33 +0530

    model train & evaluation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnClassifier.logging import logger
 from cnnClassifier.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipelines.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipelines.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipelines.stage_04_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = 'data ingestion stage'
 
@@ -21,6 +23,30 @@ try:
     logger.info(f'>>>>>> stage %s started <<<<<<<' % STAGE_NAME)
     data_ingestion = PrepareBaseModelTrainingPipeline()
     data_ingestion.main()
+    logger.info(f'>>>>>> stage %s completed <<<<<<< \n\nx==========x' % STAGE_NAME)
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'model training'
+
+try:
+    logger.info(f'>>>>>> stage %s started <<<<<<<' % STAGE_NAME)
+    model_training = ModelTrainingPipeline()
+    model_training.main()
+    logger.info(f'>>>>>> stage %s completed <<<<<<< \n\nx==========x' % STAGE_NAME)
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'model evaluation'
+
+try:
+    logger.info(f'>>>>>> stage %s started <<<<<<<' % STAGE_NAME)
+    prepare_base_model = ModelEvaluationTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f'>>>>>> stage %s completed <<<<<<< \n\nx==========x' % STAGE_NAME)
 
 except Exception as e:
--- src/cnnClassifier/pipelines/stage_04_evaluation.py
+++ src/cnnClassifier/pipelines/stage_04_evaluation.py
@@ -1,27 +1,27 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier.components.evaluation import Evaluation
 from cnnClassifier.logging import logger
 
-STAGE_NAME = 'prepare base model'
+STAGE_NAME = 'model evaluation'
 
-class PrepareBaseModelTrainingPipeline:
+class ModelEvaluationTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
 
         config = ConfigurationManager()
-        prepare_base_model_config = config.get_prepare_base_model_config()
-        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
-        prepare_base_model.get_base_model()
-        prepare_base_model.update_base_model()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
 
 
 if __name__ == '__main__':
 
     try:
         logger.info(f'>>>>>> stage %s started <<<<<<<' % STAGE_NAME)
-        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model = ModelEvaluationTrainingPipeline()
         prepare_base_model.main()
         logger.info(f'>>>>>> stage %s completed <<<<<<< \n\nx==========x' % STAGE_NAME)
 
